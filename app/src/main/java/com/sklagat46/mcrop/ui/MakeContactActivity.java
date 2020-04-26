5746a12d110df039a9c891ebe03ef7224a0643bb
sklagat45 <sklagat46@gmail.com>
2020-04-26 21:10:11 +0300

    created make contact

---
+++ app/src/main/java/com/sklagat46/mcrop/ui/MakeContactActivity.java
@@ -0,0 +1,16 @@
+package com.sklagat46.mcrop.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sklagat46.mcrop.R;
+
+public class MakeContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_contact);
+    }
+}
